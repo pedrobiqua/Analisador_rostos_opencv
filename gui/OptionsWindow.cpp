b15a0ca4c48970da69e53520e2cf587d80430e02
Pedro Bianchini de Quadros <pedrobiqua@gmail.com>
2023-12-21 15:49:52 -0300

    Update OptionsWindow

--- gui/OptionsWindow.cpp
+++ gui/OptionsWindow.cpp
@@ -8,7 +8,6 @@ OptionsWindow::OptionsWindow() : m_box(Gtk::Orientation::ORIENTATION_VERTICAL),
                                  m_label_folder("Pasta de Leitura:"),
                                  m_label_model("Modelo Selecionado:"),
                                  m_button_browse_folder("Procurar..."),
-                                 m_button_select_model("Selecionar..."),
                                  m_button_apply("Aplicar")
 {
     set_hierarchy();
@@ -17,23 +16,96 @@ OptionsWindow::OptionsWindow() : m_box(Gtk::Orientation::ORIENTATION_VERTICAL),
 }
 
 void OptionsWindow::set_hierarchy(){
-    add(m_box);
+
+    // Window
+    add(m_fixed);
+
+    // Fixed
+    m_fixed.add(m_frame);
+
+    // Frame
+    m_frame.add(m_box);
+
+    // Box
     m_box.pack_start(m_label_folder, Gtk::PACK_SHRINK);
     m_box.pack_start(m_entry_folder, Gtk::PACK_EXPAND_WIDGET);
     m_box.pack_start(m_button_browse_folder, Gtk::PACK_SHRINK);
+
     m_box.pack_start(m_label_model, Gtk::PACK_SHRINK);
-    m_box.pack_start(m_entry_model, Gtk::PACK_EXPAND_WIDGET);
-    m_box.pack_start(m_button_select_model, Gtk::PACK_SHRINK);
+    m_box.pack_start(m_combo_model, Gtk::PACK_SHRINK);
+
     m_box.pack_start(m_button_apply, Gtk::PACK_SHRINK);
 
+    // Posicionamento
+    m_fixed.move(m_frame, 10, 10);
+
     m_button_browse_folder.signal_clicked().connect(sigc::mem_fun(*this, &OptionsWindow::on_button_browse_folder_clicked));
-    m_button_select_model.signal_clicked().connect(sigc::mem_fun(*this, &OptionsWindow::on_button_select_model_clicked));
     m_button_apply.signal_clicked().connect(sigc::mem_fun(*this, &OptionsWindow::on_button_apply_clicked));
 }
 
 void OptionsWindow::draw_widgets() {
+    Config config;
+    auto configurations = config.read_config("../config.ini");
+
     set_title("Opções");
-    set_default_size(400, 200);
+    set_default_size(600, 400);
+
+    // Frame
+    m_frame.set_size_request(580, 380);
+    m_frame.set_shadow_type(Gtk::ShadowType::SHADOW_ETCHED_IN);
+
+    // Box
+    m_box.set_spacing(10);
+    m_box.set_halign(Gtk::ALIGN_CENTER);
+    m_box.set_valign(Gtk::ALIGN_CENTER);
+
+    // Entry
+    m_entry_folder.set_editable(false);
+    m_entry_folder.set_visible(true);
+    m_entry_folder.set_can_focus(true);
+    // m_entry_folder.set_placeholder_text("Selecione uma pasta");
+    m_entry_folder.set_text(configurations["Images"]["Folder_path"]);
+
+    // Combo Model
+    m_combo_model.set_visible(true);
+    m_combo_model.set_can_focus(true);
+    auto files = reader_files::ReaderFilesInFolder::read_files_in_folder("../model");
+    for(auto& file : files){
+        m_combo_model.append(file);
+    }
+
+    // Especifica o nome do arquivo que você deseja pré-selecionar
+    Glib::ustring file_selected = configurations["Model"]["Model_name"];
+
+    // Procura o índice do arquivo na lista
+    int index = -1;
+    for (size_t i = 0; i < files.size(); ++i) {
+        if (files[i] == file_selected) {
+            index = static_cast<int>(i);
+            break;
+        }
+    }
+
+    // Se encontrado, define o índice como ativo
+    if (index != -1) {
+        m_combo_model.set_active(index);
+    }
+
+    // Button Browse Folder
+    m_button_browse_folder.set_visible(true);
+    m_button_browse_folder.set_can_focus(true);
+
+    // Label Folder
+    m_label_folder.set_visible(true);
+    m_label_folder.set_can_focus(true);
+
+    // Label Model
+    m_label_model.set_visible(true);
+    m_label_model.set_can_focus(true);
+
+    // Button Apply
+    m_button_apply.set_visible(true);
+    m_button_apply.set_can_focus(true);
 }
 
 void OptionsWindow::on_button_browse_folder_clicked() {
@@ -49,12 +121,35 @@ void OptionsWindow::on_button_browse_folder_clicked() {
     }
 }
 
-void OptionsWindow::on_button_select_model_clicked() {
-    std::cout << "Seleção do modelo" << std::endl;
+void OptionsWindow::on_button_apply_clicked() {
+    Glib::ustring folder_selected = m_entry_folder.get_text();
+    Glib::ustring active_text = m_combo_model.get_active_text();
+
+    std::cout << "Pasta Selecionada: " << folder_selected << std::endl;
+    std::cout << "Model Selecionado: " << active_text << std::endl;
+
+    if (show_yes_no_dialog("Você deseja continuar?", *this)) {
+        std::cout << "Usuário clicou em Sim." << std::endl;
+        Config config;
+        std::string nomeArquivo = "../config.ini";
+        std::map<std::string, std::map<std::string, std::string>> configuracao = config.read_config(nomeArquivo);
+
+        config.edit_config(configuracao, "Images", "Folder_path", folder_selected);
+        config.edit_config(configuracao, "Model", "Model_name", active_text);
+
+        config.save_config(nomeArquivo, configuracao);
+
+        // std::cout << "Model_folder: " << configuracao["Model"]["Model_name"] << std::endl;
+        // std::cout << "Img_folder: " << configuracao["Images"]["Folder_path"] << std::endl;
+    }
 }
 
-void OptionsWindow::on_button_apply_clicked() {
-    // Adicione aqui o código para aplicar as opções
-    std::cout << "Pasta Selecionada: " << m_entry_folder.get_text() << std::endl;
-    std::cout << "Modelo Selecionado: " << m_entry_model.get_text() << std::endl;
+bool OptionsWindow::show_yes_no_dialog(const Glib::ustring &message, Gtk::Window &parent) {
+
+    Gtk::MessageDialog dialog(message, false, Gtk::MESSAGE_QUESTION, Gtk::BUTTONS_YES_NO, true);
+    dialog.set_transient_for(parent);
+
+    int result = dialog.run();
+    return (result == Gtk::RESPONSE_YES);
+
 }
--- gui/OptionsWindow.hpp
+++ gui/OptionsWindow.hpp
@@ -7,12 +7,18 @@
 
 #include <gtkmm.h>
 #include <iostream>
+#include "../util/ReaderFilesInFolder.h"
+#include "../util/Config.hpp"
 
 class OptionsWindow : public Gtk::Window {
+    Gtk::Fixed m_fixed;
+    Gtk::Frame m_frame;
     Gtk::Box m_box;
     Gtk::Label m_label_folder, m_label_model;
-    Gtk::Entry m_entry_folder, m_entry_model;
-    Gtk::Button m_button_browse_folder, m_button_select_model, m_button_apply;
+    Gtk::Entry m_entry_folder;
+    Gtk::ComboBoxText m_combo_model;
+    Gtk::Button m_button_browse_folder, m_button_apply;
+
 public:
     OptionsWindow();
 
@@ -20,8 +26,8 @@ private:
     void set_hierarchy();
     void draw_widgets();
     void on_button_browse_folder_clicked();
-    void on_button_select_model_clicked();
     void on_button_apply_clicked();
+    bool show_yes_no_dialog(const Glib::ustring& message, Gtk::Window& parent);
 
 
 };
